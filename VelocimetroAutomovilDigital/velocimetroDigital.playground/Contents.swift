f9ca7e62e7052b108452cc512aa9af7fcf642e53
josman185hotm <josman185@gmail.com>
2016-06-18 22:11:58 -0400

    VelocimetroDigital

---
+++ VelocimetroAutomovilDigital/velocimetroDigital.playground/Contents.swift
@@ -0,0 +1,63 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+class Auto {
+    var velocidad = Velocidades(velocidadInicial: .Apagado)
+    
+    init(velocidad: Velocidades){
+        self.velocidad = velocidad
+    }
+    
+    func cambioDeVelocidad() -> (actual:Int , velocidadEnCadena:String) {
+        var actual : Int
+        var velocidadEnCadena : String
+        
+        switch velocidad{
+            
+        case .Apagado:
+            actual = Velocidades.Apagado.rawValue
+            velocidadEnCadena = "Apagado"
+            velocidad = .VelocidadBaja
+            break
+            
+        case .VelocidadBaja:
+            actual = Velocidades.VelocidadBaja.rawValue
+            velocidadEnCadena = "Velocidad Baja"
+            velocidad = .VelocidadMedia
+            break
+            
+        case .VelocidadMedia:
+            actual = Velocidades.VelocidadMedia.rawValue
+            velocidadEnCadena = "Velocidad Media"
+            velocidad = .VelocidadAlta
+            break
+            
+        case .VelocidadAlta:
+            actual = Velocidades.VelocidadAlta.rawValue
+            velocidadEnCadena = "Velocidad Alta"
+            velocidad = .VelocidadMedia
+            break
+        }
+        
+        return (actual,velocidadEnCadena)
+    }
+}
+
+enum Velocidades : Int {
+    case Apagado = 0
+    case VelocidadBaja = 20
+    case VelocidadMedia = 50
+    case VelocidadAlta = 120
+    
+    init(velocidadInicial : Velocidades){
+        self = .Apagado
+    }
+}
+
+var auto = Auto(velocidad: .Apagado)
+
+for i in 1...20 {
+    print(auto.cambioDeVelocidad())
+}
+
